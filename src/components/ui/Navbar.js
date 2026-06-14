a4d20a9901f1553930961cd059871f23f3ea54c9
Julen Perez Coca <julper23@gmail.com>
2026-06-14 19:01:43 +0200

    añadido workbox

--- src/components/ui/Navbar.js
+++ src/components/ui/Navbar.js
@@ -1,30 +1,44 @@
-import React from 'react'
-import { useSelector, useDispatch } from 'react-redux'
-import { startLogout } from '../../actions/auth';
-
+import React, { useEffect, useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { startLogout } from "../../actions/auth";
+ 
 export const Navbar = () => {
-
-    const dispatch = useDispatch();
-    const { name } = useSelector( state => state.auth );
-
-    const handleLogout = () => {
-        dispatch( startLogout() );
-    }
-
-    return (
-        <div className="navbar navbar-dark bg-dark mb-4">
-            <span className="navbar-brand">
-                { name }
-            </span>
-            
-            <button 
-                className="btn btn-outline-danger"
-                onClick={ handleLogout }
-            >
-                <i className="fas fa-sign-out-alt"></i>
-                <span> Salir</span>
-            </button>
-
-        </div>
-    )
-}
+  const dispatch = useDispatch();
+  const { name } = useSelector((state) => state.auth);
+  const [isOnline, setIsOnline] = useState(navigator.onLine);
+ 
+  useEffect(() => {
+    const updateOnlineStatus = () => {
+      setIsOnline(navigator.onLine);
+    };
+ 
+    window.addEventListener("online", updateOnlineStatus);
+    window.addEventListener("offline", updateOnlineStatus);
+ 
+    return () => {
+      window.removeEventListener("online", updateOnlineStatus);
+      window.removeEventListener("offline", updateOnlineStatus);
+    };
+  }, []);
+ 
+  const handleLogout = () => {
+    dispatch(startLogout());
+  };
+  console.log(isOnline);
+  
+ 
+  return (
+    <div className="navbar navbar-dark bg-dark mb-4">
+      <span className="navbar-brand">{name}</span>
+      {isOnline ? (
+        <span className="text-success">Online</span>
+      ) : (
+        <span className="text-danger">Offline</span>
+      )}
+      <button className="btn btn-outline-danger" onClick={handleLogout}>
+        <i className="fas fa-sign-out-alt"></i>
+        <span> Salir</span>
+      </button>
+    </div>
+  );
+};
